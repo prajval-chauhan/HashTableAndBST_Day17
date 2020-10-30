583dd5f910df2053d5aabf06483f4821738c02d9
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-30 21:05:20 +0530

    [Prajval] Basic Hashtable created

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HashTableAndBST_Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table Implementation");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th Index value : " +hash5);
+        }
+    }
+}
